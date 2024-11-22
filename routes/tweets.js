4b245dacce33b05dab6da2f1d5257960be7fb351
Nicolas <blanc-nicolas@outlook.com>
2024-11-22 10:50:11 +0100

    DELETE MON AJOUT

--- routes/tweets.js
+++ routes/tweets.js
@@ -16,15 +16,20 @@ router.post('/tweeter/:token', (req, res) => {
   const date = new Date()
  const newTweet = new Tweet({
         username: dataFromUser._id,
+        nickname: dataFromUser._id,
         date: date,
         content: req.body.content,
         likes: 0,
       });
 
-      newTweet.save().then(newTweet => {
-        res.json({ result: true, newTweet: newTweet });
-      });
+      newTweet.save()
+      // .then(newTweet => {
+      //   return Tweet.findById(newTweet._id) 
+      //   .populate('username') 
+      // })
+      .then(tweet => { res.json({ result: true, newTweet: tweet });
   });
+});
 });
 
 router.get('/getTweet', (req, res) => {
